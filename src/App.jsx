4db84ab862e8415998b81d4c9795bb4c89d31516
Christian Hohenbild <mail@christianhohenbild.de>
2023-12-06 13:06:24 +0100

    add example code

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,65 @@
-import { useState } from 'react'
-import Logo from '/face-blowing-a-kiss.svg'
-import './App.css'
+import * as THREE from 'three'
+import React, { Suspense, useRef, useMemo } from 'react'
+import { Canvas, extend, useThree, useLoader, useFrame } from '@react-three/fiber'
+import { OrbitControls, Sky, Text3D } from '@react-three/drei'
+import { Water } from 'three-stdlib'
 
-function App() {
-  const [count, setCount] = useState(0)
+extend({ Water })
 
+function Ocean() {
+  const ref = useRef()
+  const gl = useThree((state) => state.gl)
+  const waterNormals = useLoader(THREE.TextureLoader, '/waternormals.jpeg')
+  waterNormals.wrapS = waterNormals.wrapT = THREE.RepeatWrapping
+  const geom = useMemo(() => new THREE.PlaneGeometry(10000, 10000), [])
+  const config = useMemo(
+    () => ({
+      textureWidth: 512,
+      textureHeight: 512,
+      waterNormals,
+      sunDirection: new THREE.Vector3(),
+      sunColor: 0xffffff,
+      waterColor: 0x001e0f,
+      distortionScale: 3.7,
+      fog: false,
+      format: gl.encoding
+    }),
+    [waterNormals]
+  )
+  useFrame((state, delta) => (ref.current.material.uniforms.time.value += delta))
+  return <water ref={ref} args={[geom, config]} rotation-x={-Math.PI / 2} />
+}
+
+function Box() {
+  const ref = useRef()
+  useFrame((state, delta) => {
+    ref.current.position.y = 10 + Math.sin(state.clock.elapsedTime) * 3
+    ref.current.rotation.x = ref.current.rotation.y = ref.current.rotation.z += delta
+  })
   return (
-    <>
-      <div>
-        <a>
-          <img src={Logo} className="logo" alt="Kiss" />
-        </a>
-      </div>
-      <h1>Template</h1>
-    </>
+    
+    <mesh ref={ref} scale={10}>
+      <boxGeometry />
+      {/* <Text3D>
+      Testing
+     </Text3D> */}
+      <meshNormalMaterial />
+    </mesh>
+  
   )
 }
 
-export default App
+export default function App() {
+  return (
+    <Canvas camera={{ position: [0, 5, -100], fov: 55, near: 1, far: 20000 }}>
+      <pointLight position={[100, 100, 100]} />
+      <pointLight position={[-100, -100, -100]} />
+      <Suspense fallback={null}>
+        <Ocean />
+        <Box />
+      </Suspense>
+      <Sky scale={1000} sunPosition={[500, 150, -1000]} turbidity={0.1} />
+      <OrbitControls />
+    </Canvas>
+  )
+}
